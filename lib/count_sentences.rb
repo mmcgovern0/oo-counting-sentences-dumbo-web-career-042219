1eb26e649d5aa2eca16c6a6d0dd6500c70cf257d
mmcgovern0 <mmcgovern.wework@gmail.com>
2019-04-24 16:49:36 -0400

    Counted Sentences

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+  	self.end_with?("?")
   end
 
   def exclamation?
-
+  	self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+  	words = self.split(" ")
+  	i = 0
+  	words.each do |words|
+  		if words.sentence? || words.question? || words.exclamation? 
+  			i +=1
+  		end
+  	end
+  	i
   end
+
 end
